4a8dbc44bd8b9d2651eebc689720cb414fd2b2c1
patkarisushanth <55051821+patkarisushanth@users.noreply.github.com>
2020-02-26 22:04:35 +0530

    Add files via upload

---
+++ ConstructionCostEstimator/src/main/java/com/sushanth/ConstructionCostEstimator/App.java
@@ -0,0 +1,13 @@
+package com.sushanth.ConstructionCostEstimator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //
+    }
+}
---
+++ InterestCalculatorEstimator/src/main/java/com/sushanth/InterestCalculator/App.java
@@ -0,0 +1,10 @@
+package com.sushanth.InterestCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+}
